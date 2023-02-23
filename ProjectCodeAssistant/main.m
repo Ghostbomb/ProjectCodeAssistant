9170460e482a55cfbdf16409e12cce282565314c
Pavlo Havrylyuk <12107961+Ghostbomb@users.noreply.github.com>
2023-02-22 19:53:17 -0800

    Initial Implementation of Socket Communication

--- ProjectCodeAssistant/main.m
+++ ProjectCodeAssistant/main.m
@@ -7,9 +7,41 @@
 
 #import <Cocoa/Cocoa.h>
 
+#import "SocketClient.h"
+
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // Setup code that might create autoreleased objects goes here.
+        
     }
+    SocketClient *socketClient = [[SocketClient alloc] init];
+        int socketDescriptor = [socketClient connectToServerWithIP:@"10.0.0.142" andPort:9876];
+        NSLog(@"Socket descriptor: %d", socketDescriptor);
+        if (socketDescriptor != -1) {
+            NSString *message = @"Hello, server!";
+            NSData *dataToSend = [message dataUsingEncoding:NSUTF8StringEncoding];
+            BOOL success = [socketClient sendData:dataToSend onSocket:socketDescriptor];
+
+            if (success) {
+                NSLog(@"Sent data successfully");
+            }
+            else {
+                NSLog(@"Failed to send data");
+            }
+        }
+        else {
+            NSLog(@"Failed to connect to server");
+        }
+        // Receive/print data from the socket server
+        NSData *dataReceived = [socketClient receiveDataOnSocket:socketDescriptor];
+        if (dataReceived) {
+            NSString *message = [[NSString alloc] initWithData:dataReceived encoding:NSUTF8StringEncoding];
+            NSLog(@"Received data: %@", message);
+        }
+        else {
+            NSLog(@"Failed to receive data");
+        }
+        
+    
     return NSApplicationMain(argc, argv);
 }
