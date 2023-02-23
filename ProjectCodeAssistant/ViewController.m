9cac45786f83e6e39467058e60597bf9af03ec0b
Pavlo Havrylyuk <12107961+Ghostbomb@users.noreply.github.com>
2023-02-23 14:19:55 -0800

    Restructure and extract functions

--- ProjectCodeAssistant/ViewController.m
+++ ProjectCodeAssistant/ViewController.m
@@ -68,43 +68,85 @@
 }
 
 
-- (IBAction)runCreateProject:(NSButton *)sender {
-    [self clearLogTextField];
-    [self appendTextToTextField:@"Creating Project..."];
-    [self appendTextToTextField:@"Connecting to Microservice..."];
-    SocketClient *socketClient = [[SocketClient alloc] init];
-    int socketDescriptor = [socketClient connectToServerWithIP:@"127.0.0.1" andPort:9876];
-    if (socketDescriptor == -1) {
-        [self appendTextToTextField:@"Failed to connect to Microservice"];
-        return;
-    }else{
-        [self appendTextToTextField:@"Connected to Microservice"];
+static void createFolder(ViewController *object, NSString *userLocation, NSString *userName) {
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSString *folderPath = [userLocation stringByAppendingPathComponent:userName];
+    NSError *error;
+    BOOL folderCreationSuccess = [fileManager createDirectoryAtPath:folderPath withIntermediateDirectories:YES attributes:nil error:&error];
+    if (folderCreationSuccess) {
+        NSLog(@"Folder created successfully");
+        [object appendTextToTextField:@"Folder created successfully"];
+        [object appendTextToTextField:folderPath];
+        [[Globals sharedInstance] setLastCreatedProject:folderPath];
     }
+    else {
+        NSLog(@"Error creating folder: %@", error.localizedDescription);
+        [object appendTextToTextField:@"Error creating folder"];
+        return;
+    }
+}
 
-    //Grab data and convert to JSON with proper bool values/conversions
-    NSString *userName = _userProjectName.stringValue;
+static void buildJSONData(ViewController *object, NSDictionary **jsonData, NSString **userLocation, NSString **userName) {
+    *userName = object->_userProjectName.stringValue;
     NSString *userDescription = @"test";
-    BOOL userPrivateGit = _userPrivateGit.state;
-    NSString *userBranchName = _userBranchName.stringValue;
-    NSString *userLocation = _userLocation.stringValue;
-    BOOL userPublishRepo = _userPublishRepo.state;   
-
-
-    NSDictionary *jsonData = @{
-        @"name": userName,
+    BOOL userPrivateGit = object->_userPrivateGit.state;
+    NSString *userBranchName = object->_userBranchName.stringValue;
+    *userLocation = object->_userLocation.stringValue;
+    BOOL userPublishRepo = object->_userPublishRepo.state;
+    
+    
+    *jsonData = @{
+        @"name": *userName,
         @"description": userDescription,
         @"private": @(userPrivateGit),
         @"branch": userBranchName,
-        @"location": userLocation,
+        @"location": *userLocation,
         @"publish": @(userPublishRepo)
     };
+}
 
+static void sendJSONData(ViewController *object, NSDictionary *jsonData, SocketClient *socketClient, int socketDescriptor) {
     NSData *jsonDataToSend = [NSJSONSerialization dataWithJSONObject:jsonData options:0 error:nil];
-    BOOL success = [socketClient sendData:jsonDataToSend onSocket:socketDescriptor];
+    BOOL sendDataSuccess = [socketClient sendData:jsonDataToSend onSocket:socketDescriptor];
+    if(sendDataSuccess){
+        [object appendTextToTextField:@"Data sent to Microservice"];
+    }else{
+        [object appendTextToTextField:@"Failed to send data to Microservice"];
+        return;
+    }
+}
 
-    //Once confimation recieved
-    // [[Globals sharedInstance] setLastCreatedProject:userLocation];
+static void connectToSocket(ViewController *object, SocketClient **socketClient, int *socketDescriptor) {
+    [object appendTextToTextField:@"Connecting to Microservice..."];
+    *socketClient = [[SocketClient alloc] init];
+    *socketDescriptor = [*socketClient connectToServerWithIP:@"127.0.0.1" andPort:9876];
+    if (*socketDescriptor == -1) {
+        [object appendTextToTextField:@"Failed to connect to Microservice"];
+        //        return;
+    }else{
+        [object appendTextToTextField:@"Connected to Microservice"];
+    }
+}
+
+- (IBAction)runCreateProject:(NSButton *)sender {
+    [self clearLogTextField];
+    [self appendTextToTextField:@"Creating Project..."];
+    // Connect
+    SocketClient * socketClient;
+    int socketDescriptor;
+    connectToSocket(self, &socketClient, &socketDescriptor);
+
+    //Grab data and convert to JSON with proper bool values/conversions
+    NSString * userName;
+    NSString * userLocation;
+    NSDictionary * jsonData;
+    buildJSONData(self, &jsonData, &userLocation, &userName);
+
+    // Create a folder with the name of the project
+    createFolder(self, userLocation, userName);
     
+    // Send Data
+    sendJSONData(self, jsonData, socketClient, socketDescriptor);
 }
 
 - (IBAction)userUndoButton:(NSButton *)sender {
@@ -124,7 +166,10 @@
         BOOL success = [fileManager trashItemAtURL:[NSURL fileURLWithPath:folderPath] resultingItemURL:nil error:&error];
         if (success) {
             NSLog(@"Folder moved to trash successfully");
+            NSLog(@"Folder path: %@", folderPath);
             [self appendTextToTextField:@"Folder moved to trash successfully"];
+            [self appendTextToTextField:folderPath];
+            
         }
         else {
             NSLog(@"Error moving folder to trash: %@", error.localizedDescription);
